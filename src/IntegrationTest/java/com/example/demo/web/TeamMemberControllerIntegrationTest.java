3379d66785d8a2aa134e66a61b49b51f37096e5c
Tomasz Piczak <tomaszpiczak@Tomasz-Piczaks-MacBook-Pro.local>
2020-03-23 16:52:53 -0500

    Demo prep

--- src/IntegrationTest/java/com/example/demo/web/TeamMemberControllerIntegrationTest.java
+++ src/IntegrationTest/java/com/example/demo/web/TeamMemberControllerIntegrationTest.java
@@ -1,18 +1,10 @@
 package com.example.demo.web;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,32 +14,4 @@ class TeamMemberControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void shouldReturnAllTeamMembers_asJSON() throws Exception {
-        wms.start();
-        wms.stubFor(WireMock.get(urlEqualTo("/getTeamAssignment/jlowrey"))
-                .willReturn(WireMock.aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("{\"teamName\":\"Sentinel\"}")));
-
-        mockMvc.perform(get("/getAllTeamMembers"))
-                .andExpect(status().isOk())
-               .andExpect(content().json("[\n" +
-                        "  {\n" +
-                        "    \"userId\": \"tpicza1\",\n" +
-                        "    \"firstName\": \"Tom\",\n" +
-                        "    \"lastName\": \"Piczak\",\n" +
-                        "    \"team\": \"Starflake\"\n" +
-                        "  },\n" +
-                        "  " +
-                        "{\n" +
-                        "    \"userId\": \"jlowrey\",\n" +
-                        "    \"firstName\": \"Jon\",\n" +
-                        "    \"lastName\": \"Lowrey\",\n" +
-                        "    \"team\": \"Sentinel\"\n" +
-                        "  }\n" +
-                        "]"));
-        wms.stop();
-    }
 }
--- src/main/java/com/example/demo/service/TeamMemberService.java
+++ src/main/java/com/example/demo/service/TeamMemberService.java
@@ -1,31 +1,17 @@
 package com.example.demo.service;
 
 import com.example.demo.common.TeamMember;
-import com.example.demo.data.TeamMemberRepository;
-import com.example.demo.provider.TeamNameProvider;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-
-import static org.springframework.util.StringUtils.isEmpty;
 
 @Service
 public class TeamMemberService {
 
-    TeamMemberRepository teamMemberRepository;
-    TeamNameProvider teamNameProvider;
-
-    public TeamMemberService(TeamMemberRepository repository, TeamNameProvider teamNameProvider) {
-        teamMemberRepository = repository;
-        this.teamNameProvider = teamNameProvider;
+    public TeamMemberService() {
     }
 
     public List<TeamMember> getAllTeamMembers() {
-        List<TeamMember> list = Optional.ofNullable(teamMemberRepository.findAll()).orElse(Collections.emptyList());
-        list.stream().filter(teamMember -> isEmpty(teamMember.team))
-                .forEach(teamMember -> teamMember.team = teamNameProvider.getTeamName(teamMember.userId));
-        return list;
+         return null;
     }
 }
